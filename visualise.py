b4813daa518a1087f6a0d9d4cc9a5c14115c84ee
7sujit <muduli_sk@yahoo.com>
2016-04-27 16:32:43 +0530

    Project Completed

--- visualise.py
+++ visualise.py
@@ -6,21 +6,19 @@ import matplotlib.pyplot as plt
 
 def draw_graph(node_pairs):
     G = nx.Graph()
+    offset=0.1
     # print "length is %s\n" % length
     # node_list = [item for item in _list] # item VARIABLE MUST BE A STRING
     for item in node_pairs:
         G.add_edge(item[0], item[1])
-    pos = nx.circular_layout(G)
-    draw_lifted(G, pos)     #DRAW LIFETED LABLES
-
-
-def draw_lifted(G, pos=None, offset=0.08, fontsize=8):
-    pos = nx.circular_layout(G) if pos is None else pos
-    nx.draw(G, pos, font_size = fontsize, with_labels=False, node_color='yellow')
+    pos = nx.shell_layout(G)
+    nx.draw(G, pos, font_size = 8, with_labels=False, node_color='m')
     for p in pos:
         pos[p][1] -= offset
-    nx.draw_networkx_labels(G,pos,offset=0.08,font_size=10,font_color='b' )
+    nx.draw_networkx_labels(G,pos,font_size=10,font_color='b' )
     plt.show()
+
+
 
 if __name__ == '__main__':
     array = [ str(i) for i in range(3)]
